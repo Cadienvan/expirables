f64a107eceb60199c593d1eb814c5079f6fcd17e
Michael Di Prisco <cadienvan@gmail.com>
2023-01-24 15:20:36 +0100

    test: improved coverage to 100% lines

--- src/LinkedList/index.test.ts
+++ src/LinkedList/index.test.ts
@@ -10,17 +10,52 @@ describe('ExpirableLinkedList', () => {
     expect(list.head!.next!.next!.value).toBe(3);
   });
 
-  it('should prepend an element', () => {
-    const list = new ExpirableLinkedList([1, 2, 3]);
-    list.prepend(4);
+  it('should prepend an element in both an empty and filled list', () => {
+    const list = new ExpirableLinkedList([1, 2, 3, 4]);
+    list.prepend(4, 1000); // 4 will expire after 1s, adding second parameter only for coverage
     expect(list.head!.value).toBe(4);
     expect(list.head!.next!.value).toBe(1);
     expect(list.head!.next!.next!.value).toBe(2);
     expect(list.head!.next!.next!.next!.value).toBe(3);
+    const list2 = new ExpirableLinkedList();
+    list2.prepend(4);
+    expect(list2.head!.value).toBe(4);
+  });
+
+  it('should clear the list', () => {
+    const list = new ExpirableLinkedList([1, 2, 3]);
+    expect(list.length).toBe(3);
+    list.clear();
+    expect(list.length).toBe(0);
+  });
+
+  it('should return false when removing an element that does not exist', () => {
+    const list = new ExpirableLinkedList([1, 2, 3]);
+    expect(list.remove(Symbol())).toBe(false);
+  });
+
+  it('should keep alive an element when setExpiration is called again', async () => {
+    const list = new ExpirableLinkedList([1, 2, 3], {
+      defaultTtl: 10,
+      unrefTimeouts: true
+    });
+    expect(list.length).toBe(3);
+    await sleep(5);
+    list.setExpiration(list.head!, 20);
+    await sleep(10);
+    expect(list.length).toBe(2);
+    list.setExpiration(list.head!, 20);
+    await sleep(10);
+    expect(list.length).toBe(1);
+    await sleep(10);
+    expect(list.length).toBe(0);
   });
 
   it('should remove the first element after the expiration time', async () => {
-    const list = new ExpirableLinkedList([1, 2, 3], { defaultTtl: 10 });
+    const list = new ExpirableLinkedList([1, 2, 3], {
+      defaultTtl: 10,
+      unrefTimeouts: true
+    });
     await sleep(20);
     expect(list.head).toBeNull();
   });
@@ -32,6 +67,19 @@ describe('ExpirableLinkedList', () => {
     expect(list.length).toBe(0);
     list.append(4, 30);
     expect(list.length).toBe(1);
+    await sleep(20);
+    expect(list.length).toBe(1);
+    await sleep(20);
+    expect(list.length).toBe(0);
+  });
+
+  it('should allow entries to define a specific ttl and let them expire accordingly', async () => {
+    const list = new ExpirableLinkedList([[1, 30], 2, [3, 50]], {
+      defaultTtl: 10
+    }); // 2 will expire after 10ms, 1 after 30ms and 3 after 50ms
+    expect(list.length).toBe(3);
+    await sleep(11);
+    expect(list.length).toBe(2);
     await sleep(20);
     expect(list.length).toBe(1);
     await sleep(20);
--- src/Queue/index.test.ts
+++ src/Queue/index.test.ts
@@ -11,6 +11,22 @@ describe('ExpirableQueue', () => {
     expect(queue.dequeue()).toBeUndefined();
   });
 
+  it('should return the correct next element', () => {
+    const queue = new ExpirableQueue([1, 2, 3]);
+    expect(queue.next.value).toBe(1);
+  });
+
+  it('should allow entries to define a specific ttl and let them expire accordingly', async () => {
+    const queue = new ExpirableQueue([[1, 30], 2, [3, 50]], { defaultTtl: 10 }); // 2 will expire after 10ms, 1 after 30ms and 3 after 50ms
+    expect(queue.size).toBe(3);
+    await sleep(11);
+    expect(queue.size).toBe(2);
+    await sleep(20);
+    expect(queue.size).toBe(1);
+    await sleep(20);
+    expect(queue.size).toBe(0);
+  });
+
   it('should remove the first element after the expiration time', async () => {
     const queue = new ExpirableQueue([1, 2, 3], { defaultTtl: 10 });
     expect(queue.dequeue()).toBe(1);
